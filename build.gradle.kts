f9f2161810e95639f29448a9dd8e51c30185ecf7
ding <ding@ding.com>
2026-09-11 20:32:57 +0800

    补齐后端技术基线

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,10 @@ plugins {
 
 repositories { mavenCentral() }
 
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:3.0.3")
+    implementation("org.postgresql:postgresql:42.7.4")
+    implementation("io.lettuce:lettuce-core:6.5.1.RELEASE")
+}
+
 application { mainClass.set("MainKt") }
